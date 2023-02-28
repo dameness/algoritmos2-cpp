bf3be003acc412740a797af29b82b7aef41af363
Bernardo Damiani <bmdamiani17@gmail.com>
2023-02-28 14:00:07 -0300

    ordenação de vetores e uso de algumas funções da cmath

--- atividade_1_bernardo_damiani/ex2.cpp
+++ atividade_1_bernardo_damiani/ex2.cpp
@@ -1,60 +1,64 @@
 #include <iostream>
+#include <ctime>
+#include <cmath>
 using namespace std;
 
+#define tam 20
+
 /*
-Escreva um programa que simule um caixa eletrônico que dispõe das seguintes notas: 2,
-5, 10, 20, 50, 100 e 200 o programa deverá receber o valor do saque e, sendo possível realizar
-o saque, deverá apresentar a quantidade e quais notas devem ser entregues, sempre
-priorizando as notas maiores.
-Não é permitido saque de valores menores que 2 e igual a 3 os demais valores é possível
-realizar o saque:
+Escreva um programa que manipule um vetor VET[20] de números float, os valores para
+preencher o vetor devem ser gerados randomicamente, mostre o vetor gerado na tela, exibindo
+5 valores em cada linha e separando cada valor com uma vírgula, a seguir mostre o vetor com
+valores arredondados para cima, para baixo e seguindo o critério da primeira casa decimal.
 */
+void exibir(float vetor[], int tamanho);
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int valor, n200, n100, n50, n20, n10, n5, n2;
+    float vet[tam], vet_ceil[tam], vet_floor[tam], vet_round[tam];
 
-    do
-    {
-        system("cls");
+    cout << fixed;
+    cout.precision(2);
 
-        cout << "Informe um valor para sacar em notas: ";
-        cin >> valor;
-        fflush(stdin);
+    for(int i=0; i<tam; i++)
+        vet[i] = rand()%101 * 1.36; // limitei os valores até 100 para facilitar a visualização
+        // o número gerado pela função rand() é inteiro; multipliquei por um float aleatório para convertê-lo para float
 
-        if(valor==3 || valor < 2)
-        {
-            cout << "\nValor inválido! Insira um valor válido!\n\n";
-            system("pause");
-        }
+    cout << "Vetor gerado:\n";
+    exibir(vet, tam);
 
-    }while(valor==3 || valor<2);
+    cout.precision(0);
 
-    n200 = valor / 200;
-    valor %= 200;
+    cout << "Vetor com os valores arredondados para cima:\n";
+    for(int i=0; i<tam; i++)
+        vet_ceil[i] = ceil(vet[i]);
+    exibir(vet_ceil, tam);
 
-    n100 = valor / 100;
-    valor %= 100;
+    cout << "Vetor com os valores arredondados para baixo:\n";
+    for(int i=0; i<tam; i++)
+        vet_floor[i] = floor(vet[i]);
+    exibir(vet_floor, tam);
 
-    n50 = valor / 50;
-    valor %= 50;
-
-    n20 = valor / 20;
-    valor %= 20;
-
-    n10 = valor / 10;
-    valor %= 10;
-
-    n5 = valor / 5;
-    valor %= 5;
-
-    n2 = valor / 2;
-    valor %= 2;
-
-    printf("Notas de 200: %d\nNotas de 100: %d\nNotas de 50: %d\nNotas de 20: %d\nNotas de 10: %d\nNotas de 5: %d\nNotas de 2: %d\n", n200, n100, n50, n20, n10, n5, n2);
-
-    if(valor>0)
-        cout << "Alem disso, sera entregue uma moeda de um real.\n";
+    cout << "Vetor com os valores arredondados seguindo o critério da primeira casa decimal:\n";
+    for(int i=0; i<tam; i++)
+        vet_round[i] = round(vet[i]);
+    exibir(vet_round, tam);
 
     return 0;
+}
+
+void exibir(float vetor[], int tamanho)
+{
+    int j=1; //usado para pular a linha a cada 5 posições
+    for(int i=0; i<tamanho; i++)
+    {
+        cout << vetor[i];
+        if(i!=(tam-1))
+            cout << ", ";
+        if(i!=0 && j%5==0)
+            cout << "\n\n";
+        j++;
+    }
+    cout << "\n\n";
 }

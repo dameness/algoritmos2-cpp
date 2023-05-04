6ba91c12403a095aee06fbcbcdde4c68bcddbdd4
Bernardo Damiani <bmdamiani17@gmail.com>
2023-05-04 20:06:50 -0300

    poltronas de onibus com ponteiros

--- atividades etapa 2/atividade_8_bernardo_damiani/ex4.cpp
+++ atividades etapa 2/atividade_8_bernardo_damiani/ex4.cpp
@@ -1,62 +1,128 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 
-/*
- Crie um programa que receba um valor qualquer representando o tamanho dos
-lados de um quadrado, implemente uma função que receba esse valor e utilizando
-ponteiros escreva com o caracter “*” asterisco na tela, um quadrado com as respectivas
-diagonais,
-*/
+void venda(bool *v, char aux)
+{
 
-void leitura(int *n)
-{
-    cout << "Informe o tamanho da diagonal: ";
-    cin >> *n;
-}
-void exibir(char *vet, int tam)
-{
-    for(int i=0; i<tam; i++)
-        cout << *(vet+i) << " ";
+    int p;
+    cout << "\nEscolha o número de sua poltrona (1 a 20): ";
+    cin >> p;
 
-    cout << endl;
-}
-void quad(char *vet, int tam)
-{
-    for(int i=0; i<tam; i++)
+    if(p<1 || p>20)
+        cout << "\nPoltrona inexistente!\n";
+    else
     {
-        if(i==0 || i==tam-1)
+        p--;
+        bool inv = 1;
+
+        for(int i=0; i<20; i++)
         {
-            for(int j=0; j<tam; j++)
-                *(vet + j) = '*';
+           if(*(v+i) == 0)
+               inv = 0;
+        }
+
+        if(inv == 1)
+        {
+            cout << "\n\nO ônibus está lotado nas poltronas ";
+            if(aux=='j')
+                cout << "da janela!\n";
+            else
+                cout << "do corredor!\n";
+        }
+        else if(*(v + p) == 1)
+        {
+            cout << "\nPoltrona ocupada!\n";
         }
         else
         {
-            for(int j=0; j<tam; j++)
-            {
-                *(vet + j) = ' ';
-            }
-            for(int j=0; j<tam; j++)
-            {
-                if(j==0 || j==tam-1)
-                    *(vet + j) = '*';
-                else if(j==i)
-                {
-                    *(vet + j) = '*';
-                    *(vet + (tam-1) - j) = '*';
-                }
-            }
+            *(v + p) = 1;
+            cout << "\nVenda efetivada!\n";
         }
-
-        exibir(vet, tam);
     }
+
+
+    system("pause");
+
+}
+void ocupacao(bool *j, bool *c)
+{
+    cout << "\n\nJANELA\t\tCORREDOR\n\n";
+    for(int i=0; i<20; i++)
+    {
+        cout << i+1 << "- ";
+        if(*(j+i)==1)
+            cout << "Ocupada";
+        else
+            cout << "Livre";
+        cout << "\t\t";
+        cout << i+1 << "- ";
+        if(*(c+i)==1)
+            cout << "Ocupada";
+        else
+            cout << "Livre";
+            cout << "\n";
+    }
+    cout << "\n\n";
 }
 main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int n;
-    leitura(&n);
-    char vet[n];
+    bool janela[20], corredor[20];
 
-    quad(vet, n);
+    for(int i=0; i<20; i++)
+    {
+        *(janela + i) = 0;
+        *(corredor + i) = 0;
+    }
+
+    int op;
+    do
+    {
+
+        system("cls");
+        cout << "Digite uma opção:\n1 - vender passagem\n2 - mostrar mapa de ocupação do ônibus\n3 - encerrar\n\n\tOpção: ";
+        cin >> op;
+
+        if(op<1 || op>3)
+        {
+            cout << "\nOpção inválida! Digite novamente!\n";
+            system("pause");
+        }
+        else if(op==1)
+        {
+            char aux;
+            do
+            {
+                cout << "\n\nEscolha uma opção:\nJ - janela\nC - corredor\n\n\tOpção: ";
+                cin >> aux;
+
+                aux = tolower(aux);
+
+                if(aux!='j' && aux!='c')
+                {
+                    cout << "\nOpção inválida! Digite novamente!\n";
+                    system("pause");
+                }
+
+            }while(aux!='j' && aux!='c');
+
+            if(aux=='j')
+                venda(janela, aux);
+            else
+                venda(corredor, aux);
+
+
+        }
+        else if(op==2)
+        {
+            ocupacao(janela, corredor);
+            system("pause");
+        }
+        else if(op==3)
+        {
+            cout << "\nO programa será encerrado!\n";
+        }
+
+    }
+    while(op!=3);
 }
